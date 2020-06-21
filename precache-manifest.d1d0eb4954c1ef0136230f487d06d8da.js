81f0dea1ad952c24c6e8892443b5b1af35d25e47
root <root@LAPTOP-VEULJ72R.localdomain>
2020-06-20 20:26:30 -0400

    Updates

---
+++ precache-manifest.d1d0eb4954c1ef0136230f487d06d8da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c2e7465043f6b41bdbe3872d3ae860",
+    "url": "/ShareEasy/index.html"
+  },
+  {
+    "revision": "0408c12b5d379b96c562",
+    "url": "/ShareEasy/static/css/main.c751c118.chunk.css"
+  },
+  {
+    "revision": "0b6f5a4f505091d39eb4",
+    "url": "/ShareEasy/static/js/2.4d073587.chunk.js"
+  },
+  {
+    "revision": "2954b1568600ec1d7b70d079ce9ac1a4",
+    "url": "/ShareEasy/static/js/2.4d073587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0408c12b5d379b96c562",
+    "url": "/ShareEasy/static/js/main.1205e963.chunk.js"
+  },
+  {
+    "revision": "aea700792e99dacc1604",
+    "url": "/ShareEasy/static/js/runtime-main.9cb961d3.js"
+  }
+]);
