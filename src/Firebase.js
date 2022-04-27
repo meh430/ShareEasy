49f13e8361036de53e2e2f20bbec8f668e10362b
meh430 <mehulpillai30@gmail.com>
2022-04-27 19:25:38 -0400

    bump

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,14 +1,14 @@
-import firebase from 'firebase'
+import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREAPI,
-    authDomain: "shareeasy-dd2ce.firebaseapp.com",
-    databaseURL: "https://shareeasy-dd2ce.firebaseio.com",
-    projectId: "shareeasy-dd2ce",
-    storageBucket: "shareeasy-dd2ce.appspot.com",
-    messagingSenderId: "999885360104",
-    appId: "1:999885360104:web:2be601450588e71272325d",
-    measurementId: "G-1H3P6BK9N7",
+  apiKey: process.env.REACT_APP_FIREAPI,
+  authDomain: "shareeasy-dd2ce.firebaseapp.com",
+  databaseURL: "https://shareeasy-dd2ce.firebaseio.com",
+  projectId: "shareeasy-dd2ce",
+  storageBucket: "shareeasy-dd2ce.appspot.com",
+  messagingSenderId: "999885360104",
+  appId: "1:999885360104:web:2be601450588e71272325d",
+  measurementId: "G-1H3P6BK9N7",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
